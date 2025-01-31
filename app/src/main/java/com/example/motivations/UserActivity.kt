bd1773c71d99b9ead1ef71837abcc55f7dbb5f47
WRamos21 <ramossantoswesley@gmail.com>
2025-01-31 17:21:02 -0300

    Navegação entre Activitys

--- app/src/main/java/com/example/motivations/UserActivity.kt
+++ app/src/main/java/com/example/motivations/UserActivity.kt
@@ -1,7 +1,9 @@
 package com.example.motivations
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,7 +16,7 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityUserBinding.inflate(layoutInflater)
+        binding = ActivityUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -27,8 +29,28 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(view: View) {
-        if (view.id == R.id.button_save){
-
+        if (view.id == R.id.button_save) {
+            handleSave()
         }
     }
+
+    /* [1.0] Navegação entre Activitys
+    salva valor em name e se for vazio mostra na tela com Toast
+    Intent carrega MainActivity mas start activity é o que leva a ela
+    finish impede o usuario de voltar a tela
+     */
+    private fun handleSave() {
+        val name = binding.editName.text.toString()
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(
+                this,
+                R.string.validation_mandatory_name,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
 }
