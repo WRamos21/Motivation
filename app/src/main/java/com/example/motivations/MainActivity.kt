3aed9e63d6121e0340b63da6f33b105f7b3685b7
WRamos21 <ramossantoswesley@gmail.com>
2025-01-31 14:47:08 -0300

    Criação da viewBinding e estilização da statusBar

--- app/src/main/java/com/example/motivations/MainActivity.kt
+++ app/src/main/java/com/example/motivations/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.motivations
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.motivations.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //Esconde barra de support
+        supportActionBar?.hide()
+
+        //events
+        binding.buttonNewPhrase.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_new_phrase){
+            var s = "a"
         }
     }
 }
